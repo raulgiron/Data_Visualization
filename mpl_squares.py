9328e007c11bc5dfa30217e2b733a0a7366746f3
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-10-12 03:12:40 -0400

    Using Built-in Styles, 'seaborn'.

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,10 @@
+import temporary
 import matplotlib.pyplot as plt
 
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+plt.style.use("seaborn")
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
