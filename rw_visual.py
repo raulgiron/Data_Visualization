974f94a4fb7418019ea672e9ec3767363297721a
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-10-16 08:08:47 -0400

    Adding Plot Points

--- rw_visual.py
+++ rw_visual.py
@@ -4,14 +4,15 @@ from random_walk import RandomWalk
 
 # Keep making new random walks, as long as the program is active.
 while True:
-    rw = RandomWalk()
+    # Make a random walk.
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
 
     # Emphasize the first and last points.
     ax.scatter(0, 0, c='green', edgecolor='none', s=100)
