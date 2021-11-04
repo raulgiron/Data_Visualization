a702cafbfa3a5124e251bc78f4da7e1fd78e1c51
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-11-04 19:45:08 -0400

    Plotting a timeframe.

---
+++ sitka_highs.py
@@ -0,0 +1,34 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+filename = "/Users/raulgiron/Desktop/Data_Visualization/data/sitka_weather_07-2018_simple.csv"
+with open(filename) as file_object:
+    reader = csv.reader(file_object)
+    header_row = next(reader)
+    # for index, column_header in enumerate(header_row, 1):
+    #     print(f"{index}-) {column_header}", end=' \t')
+
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = (int(row[5]) - 32) * 5/9
+        dates.append(current_date)
+        highs.append(high)
+    # print(highs)
+
+# Plot the high temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
+
+# Format plot.
+ax.set_title("Daily high temperatures, July 2018", fontsize=16)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel('Temperature (C)', fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
