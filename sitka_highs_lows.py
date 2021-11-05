4dbf57fe808c99f620254817665866a5fa9fcac7
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-11-05 09:06:05 -0400

    Plotting a second data series.

---
+++ sitka_highs_lows.py
@@ -0,0 +1,37 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+filename = "/Users/raulgiron/Desktop/Data_Visualization/data/sitka_weather_2018_simple.csv"
+with open(filename) as file_object:
+    reader = csv.reader(file_object)
+    header_row = next(reader)
+    # for index, column_header in enumerate(header_row, 1):
+    #     print(f"{index}-) {column_header}", end=' \t')
+
+    # Get dates and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = (int(row[5]) - 32) * 5 / 9
+        low = (int(row[6]) - 32) * 5 / 9
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+    # print(highs)
+
+# Plot the high and low temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
+
+# Format plot.
+ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel('Temperature (C)', fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
