4e0d877428a927da67d67171b9ae46f962daa4d4
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-11-07 06:06:31 -0400

    Making a list of all Earthquakes.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -5,7 +5,5 @@ import json
 filename = '/Users/raulgiron/Desktop/Data_Visualization/data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
-
-readable_file = 'data/readable_eq_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+    all_eq_dicts = all_eq_data['features']
+    print(len(all_eq_dicts))
