5fe9da1358297783a0ec6223230d84bf01ce3d68
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-11-26 20:43:35 -0400

    Summarizing the top repositories.

--- python_repos.py
+++ python_repos.py
@@ -17,8 +17,17 @@ print(f"Total repositories: {response_dict['total_count']:,.2f}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# Examine the first repository.
-repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']:,.2f} - \U00002B50")
+    print(f"Size: {repo_dict['size']/1000:.1f} MB - \U0001F4BE")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    # print(f"\nKeys: {len(repo_dict)}")
+    # for key in sorted(repo_dict.keys()):
+    #     print(key)
